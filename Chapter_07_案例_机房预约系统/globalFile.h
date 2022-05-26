bc2289a7253cd2cebd9d500301df7f8d80943196
muzing <muzi2001@foxmail.com>
2022-05-26 10:13:52 +0800

    Update Chapter_07

---
+++ Chapter_07_案例_机房预约系统/globalFile.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// 管理员文件
+#define ADMIN_FILE "./records/admin.txt"
+
+// 学生文件
+#define STUDENT_FILE "./records/students.txt"
+
+// 教师文件
+#define TEACHER_FILE "./records/teachers.txt"
+
+// 机房信息文件
+#define COMPUTER_FILE "./records/computerRooms.txt"
+
+// 订单文件
+#define ORDER_FILE "./records/order.txt"
