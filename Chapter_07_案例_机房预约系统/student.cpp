4e68cf41aa2dca21ae1aac0556725850d0362689
muzing <muzi2001@foxmail.com>
2022-05-30 16:46:58 +0800

    Update Chapter-07: 增加学生查看所有预约功能

--- Chapter_07_案例_机房预约系统/student.cpp
+++ Chapter_07_案例_机房预约系统/student.cpp
@@ -141,6 +141,42 @@ void Student::showMyOrder()
 // 查看所有预约
 void Student::showAllOrder()
 {
+    system("clear");
+    OrderFile of;
+    if (of.m_Size == 0)
+    {
+        system("clear");
+        cout << "无预约记录" << endl;
+        return;
+    }
+    for (int i = 0; i < of.m_Size; i++)
+    {
+        cout << i + 1 << ". ";
+        cout << "预约日期:周" << of.m_orderData[i]["date"];
+        cout << " 时段:" << (of.m_orderData[i]["interval"] == "1" ? "上午" : "下午");
+        cout << " 学号:" << of.m_orderData[i]["stuId"];
+        cout << " 姓名:" << of.m_orderData[i]["stuName"];
+        cout << " 机房:" << of.m_orderData[i]["roomId"];
+
+        string status = " 状态:";
+        // 0 取消的预约  1 审核中  2 已预约  -1 预约失败
+        int ofStatus = atoi(of.m_orderData[i]["status"].c_str());
+        switch (ofStatus)
+        {
+        case 1:
+            status += "审核中";
+            break;
+        case 2:
+            status += "已预约";
+            break;
+        case -1:
+            status += "预约失败";
+        default:
+            status += "预约已取消";
+            break;
+        }
+        cout << status << endl;
+    }
 }
 
 // 取消预约
