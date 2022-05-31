358c802ca352131825bfc0175274172adf423852
muzing <muzi2001@foxmail.com>
2022-05-31 11:21:03 +0800

    Update Chapter-07: 增加教师审核功能

--- Chapter_07_案例_机房预约系统/teacher.cpp
+++ Chapter_07_案例_机房预约系统/teacher.cpp
@@ -73,4 +73,60 @@ void Teacher::showAllOrder()
 // 审核预约
 void Teacher::validOrder()
 {
+    system("clear");
+    OrderFile of;
+    if (of.m_Size == 0)
+    {
+        cout << "无预约记录" << endl;
+        return;
+    }
+    cout << "待审核的预约记录如下：" << endl;
+
+    vector<int> v;
+    int index = 0;
+    for (int i = 0; i < of.m_Size; i++)
+    {
+        if (of.m_orderData[i]["status"] == "1")
+        {
+            v.push_back(i);
+            cout << ++index << "、";
+            cout << "预约日期：周" << of.m_orderData[i]["date"];
+            cout << " 时段：" << (of.m_orderData[i]["interval"] == "1" ? "上午" : "下午");
+            cout << " 机房：" << of.m_orderData[i]["roomId"];
+            cout << " 状态：审核中" << endl;
+        }
+    }
+    cout << "请输入要审核的预约记录, 0代表返回" << endl;
+    int select = 0;
+    int ret = 0;
+    while (true)
+    {
+        cin >> select;
+        if (select >= 0 && select <= v.size())
+        {
+            if (select == 0)
+            {
+                break;
+            }
+            else
+            {
+                cout << "请输入审核结果" << endl;
+                cout << "1.通过  2.不通过" << endl;
+                cin >> ret;
+
+                if (ret == 1)
+                {
+                    of.m_orderData[v[select - 1]]["status"] = "2";
+                }
+                else if (ret == 2)
+                {
+                    of.m_orderData[v[select - 1]]["status"] = "-1";
+                }
+                of.updateOrder();
+                cout << "操作成功" << endl;
+                break;
+            }
+        }
+        cout << "输入有误，请重新输入" << endl;
+    }
 }
