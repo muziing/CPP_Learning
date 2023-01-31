e9e79fbd8a13e55fc6358ed4fb4d314071441997
muzing <muzi2001@foxmail.com>
2023-01-31 09:48:09 +0800

    Update Chapter09
    增加「正向非常迭代器」的实现

--- Chapter_09_实现链表容器/list.cpp
+++ Chapter_09_实现链表容器/list.cpp
@@ -145,20 +145,124 @@ private:
     node *m_tail; // 链表尾
 
     // 在接触迭代器知识前，姑且这样用
-    friend ostream &operator<<(ostream &os, list<int> &l);
+    // friend ostream &operator<<(ostream &os, list<int> &l);
+
+public:
+    // 正向非常迭代类
+    class iterator
+    {
+    public:
+        iterator(node *start, node *cur,
+                 node *end) : m_start(start), m_cur(cur), m_end(end) {}
+        T &operator*()
+        {
+            if (m_cur == NULL)
+                throw underflow_error("null node");
+            return m_cur->m_data;
+        }
+        iterator &operator++()
+        {
+            if (m_cur == NULL)
+                m_cur = m_start;
+            else
+                m_cur = m_cur->m_next;
+            return *this;
+        }
+        iterator operator--()
+        {
+            if (m_cur == NULL)
+                m_cur = m_end;
+            else
+                m_cur = m_cur->m_prev;
+            return *this;
+        }
+        bool operator==(iterator const &that) const
+        {
+            return m_start == that.m_start && m_cur == that.m_cur && m_end == that.m_end;
+        }
+        bool operator!=(iterator const &that) const
+        {
+            return !(*this == that);
+        }
+
+    private:
+        node *m_start; // 开始指向
+        node *m_cur;   // 当前指向
+        node *m_end;   // 终止指向
+        friend class list;
+    };
+
+    // 获取起始迭代器（遍历链表）
+    iterator begin()
+    {
+        return iterator(m_head, m_head, m_tail);
+    }
+
+    // 获取终止迭代器（结束标志）
+    iterator end()
+    {
+        return iterator(m_head, NULL, m_tail);
+    }
+
+    // 迭代器指向的位置添加节点
+    void insert(iterator const &loc, T const &data)
+    {
+        if (loc == end())
+        {
+            push_back(data);
+        }
+        else
+        {
+            node *pnode = new node(loc.m_cur->m_prev, data, loc.m_cur);
+            if (pnode->m_prev)
+                pnode->m_prev->m_next = pnode;
+            else
+                m_head = pnode;
+            pnode->m_next->m_prev = pnode;
+        }
+    }
+
+    // 删除迭代器指向的节点
+    void erase(iterator const &loc)
+    {
+        if (loc == end())
+            return;
+        node *pdel = loc.m_cur;
+        if (pdel->m_prev)
+            pdel->m_prev->m_next = pdel->m_next;
+        else
+            m_head = pdel->m_next;
+        if (pdel->m_next)
+            pdel->m_next->m_prev = pdel->m_prev;
+        else
+            m_tail = pdel->m_prev;
+        delete pdel;
+    }
 };
 
 // 以上代码模拟容器
 // -----------------------
 // 以下代码模拟普通用户使用
 
-ostream &operator<<(ostream &os, list<int> &l)
+// ostream &operator<<(ostream &os, list<int> &l)
+// {
+//     for (list<int>::node *pnode = l.m_head; pnode; pnode = pnode->m_next)
+//     {
+//         os << pnode->m_data << ' ';
+//     }
+//     return os;
+// }
+
+void print(string const &str, list<int> &l)
 {
-    for (list<int>::node *pnode = l.m_head; pnode; pnode = pnode->m_next)
+    cout << str << endl;
+    typedef list<int>::iterator IT;
+    for (IT it = l.begin(); it != l.end(); ++it)
     {
-        os << pnode->m_data << ' ';
+        cout << *it << ' ';
     }
-    return os;
+    cout << endl
+         << "--------------------" << endl;
 }
 
 int main()
@@ -172,10 +276,22 @@ int main()
     {
         ls.push_back(100 + i);
     }
-    cout << ls << endl;
-
+    // cout << ls << endl;
+    print("添加节点后：", ls);
     ls.pop_front();
     ls.pop_back();
-    cout << ls << endl;
+    // cout << ls << endl;
+    print("删除头尾节点后：", ls);
+
+    ls.insert(++ls.begin(), 1000);
+    print("在迭代器指向的位置添加节点后：", ls);
+    ls.erase(ls.begin());
+    print("删除迭代器指向的节点后：", ls);
+
+    typedef list<int>::iterator IT;
+    IT it = ls.begin();
+    *it = 800;
+    print("更改迭代器指向的节点后：", ls);
+
     return 0;
 }
